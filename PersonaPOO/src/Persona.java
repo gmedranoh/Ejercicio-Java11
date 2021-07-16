adeb1fbfe33f42cf551df7d64a46a612a2e2212a
gmedranoh <55906494+gmedranoh@users.noreply.github.com>
2021-07-16 03:00:58 -0500

    PersonaPOO

---
+++ PersonaPOO/src/Persona.java
@@ -0,0 +1,15 @@
+
+public class Persona {
+	String name = "";
+	int age =0;
+	
+	/*va a tener dos metodos, uno para el name y otro age */
+	
+	public String saludar() {
+		return "hola " + this.name;
+	}
+	
+	public String miEdad() {
+		return "Mi edad es " + this.age;
+	}
+}
---
+++ PersonaPOO/src/Principal.java
@@ -0,0 +1,14 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*para instanciar*/
+		Persona gaby = new Persona();
+		gaby.name = "Gaby Medrano";
+		gaby.age = 30;
+		System.out.println(gaby.saludar());
+		System.out.println(gaby.miEdad());
+	}
+
+}
